832bc55eb085a6a1f549184fb2272d68dfa14feb
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-05-21 17:25:29 -0500

    usando ref y useRef

--- src/components/CounterEffect.tsx
+++ src/components/CounterEffect.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { gsap } from 'gsap';
 
 const MAXIMUM_COUNT = 10;
 
 const CounterEffect = () => {
   const [counter, setCounter] = useState(5);
+  const refCounter = useRef<HTMLHeadingElement>(null);
 
   const handleClick = () => {
 
@@ -16,16 +17,16 @@ const CounterEffect = () => {
 
     console.log('%cse llegó al valor máximo', 'color:withe; background:blue');
 
-    gsap.to('h2', { y: -10, ease: 'ease.out' })
+    gsap.to(refCounter.current, { y: -10, ease: 'ease.out' })
       .then(() => {
-        gsap.to('h2', { y: 0, ease: 'bounce.out' })
+        gsap.to(refCounter.current, { y: 0, ease: 'bounce.out' })
       })
   }, [counter]);
 
   return (
     <>
       <h1>Counter Effect</h1>
-      <h2>{counter}</h2>
+      <h2 ref={refCounter}>{counter}</h2>
 
       <button onClick={handleClick}>
         +1
